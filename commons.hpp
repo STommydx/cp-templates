d325f98ffb0fe0fc5520aca1a72bb8ec78097f91
STommydx <ttoo74@gmail.com>
2024-03-08 22:41:17 -0500

    chore: setup cmake project and tests

---
+++ commons.hpp
@@ -0,0 +1,43 @@
+/**
+ * @file commons.hpp
+ * @brief Provides abbreviations and other useful functions.
+ */
+
+#ifndef COMMONS_HPP
+#define COMMONS_HPP
+
+#include <utility>
+#include <vector>
+#include <queue>
+
+/**
+ * Abbreviations for common types.
+ */
+using vi = std::vector<int>;
+using vvi = std::vector<vi>;
+using vvvi = std::vector<vvi>;
+using pii = std::pair<int, int>;
+using vpii = std::vector<pii>;
+using ll = long long;
+using vll = std::vector<long long>;
+using vvll = std::vector<vll>;
+using vvvll = std::vector<vvll>;
+using pll = std::pair<ll, ll>;
+using vpll = std::vector<pll>;
+
+/**
+ * Abbreviation for the STL priority_queue with smaller element at the top.
+ */
+template <class T> using min_queue = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+
+/**
+ * Abbreviation for infinity constant.
+ */
+template <class T = int> constexpr T INFTY = std::numeric_limits<T>::has_infinity ? std::numeric_limits<T>::infinity() : std::numeric_limits<T>::max();
+template <> constexpr int INFTY<int> = 0x3f3f3f3f;
+template <> constexpr ll INFTY<ll> = 0x3f3f3f3f3f3f3f3fLL;
+template <class T, class U> constexpr std::pair<T, U> INFTY<std::pair<T, U>>{INFTY<T>, INFTY<U>};
+constexpr int INF = INFTY<>;
+constexpr ll BINF = INFTY<ll>;
+
+#endif
---
+++ commons_test.cpp
@@ -0,0 +1,60 @@
+#include "commons.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("abbreviations are defined", "[commons]") {
+    SECTION("long long datatype") {
+        ll x = 1LL << 40;
+        REQUIRE(x == 1LL << 40);
+    }
+    SECTION("mulit-dimensional vectors") {
+        vvvi three_d_matrix(1, vvi(1, vi(1, 1)));
+        REQUIRE(three_d_matrix[0][0][0] == 1);
+        vvvll three_d_ll_matrix(1, vvll(1, vll(1, 1LL << 40)));
+        REQUIRE(three_d_ll_matrix[0][0][0] == 1LL << 40);
+    }
+    SECTION("pairs") {
+        vpii vpii_vector(1, pii(1, 2));
+        vpii_vector.emplace_back(3, 4);
+        REQUIRE(vpii_vector[0].first == 1);
+        REQUIRE(vpii_vector[1].second == 4);
+        vpll vpll_vector(1, pll(1LL << 40, 2));
+        vpll_vector.emplace_back(3, 4);
+        REQUIRE(vpll_vector[0].first == 1LL << 40);
+        REQUIRE(vpll_vector[1].second == 4);
+    }
+}
+
+TEST_CASE("min_queue behaves as expected", "[commons]") {
+    min_queue<int> q;
+    q.push(5);
+    q.push(1);
+    q.push(3);
+    REQUIRE(q.top() == 1);
+    q.pop();
+    REQUIRE(q.top() == 3);
+}
+
+TEST_CASE("infinity constant is defined properly", "[commons]") {
+    SECTION("infinity is defined for mulitple types") {
+        int int_inf = INFTY<int>;
+        REQUIRE(int_inf > 0);
+        ll ll_inf = INFTY<ll>;
+        REQUIRE(ll_inf > 0LL);
+        std::pair<int, ll> pair_inf = INFTY<std::pair<int, ll>>;
+        REQUIRE(pair_inf.first > 0);
+        REQUIRE(pair_inf.second > 0LL);
+        double double_inf = INFTY<double>;
+        REQUIRE(double_inf > 1e200);
+    }
+    SECTION("infinity constants is defined for integer types") {
+        REQUIRE(INF == INFTY<int>);
+        REQUIRE(BINF == INFTY<ll>);
+    }
+    SECTION("double of integer infinity does not overflow") {
+        int double_inf = INF + INF;
+        REQUIRE(double_inf > INF);
+        ll double_ll_inf = BINF + BINF;
+        REQUIRE(double_ll_inf > BINF);
+    }
+}
