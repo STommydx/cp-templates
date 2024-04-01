389fd8cdb4426650f2b5591f37d73c95c80feeb2
STommydx <dev@stdx.space>
2024-04-01 16:43:35 -0400

    feat(templates): min cost max flow with dinic ssp

--- flow.hpp
+++ flow.hpp
@@ -90,4 +90,97 @@ template <class T> class flow_net {
 	}
 };
 
+template <class T> class cost_flow_net {
+  private:
+	struct edge {
+		T cap, flow, cost;
+	};
+	int n;
+	graph<int> g;
+	std::vector<edge> edges;
+	int source, sink;
+
+  public:
+	static constexpr int inf = cp_limits<int>::infinity();
+
+	cost_flow_net(int n, int source, int sink)
+	    : n(n), g(n), source(source), sink(sink) {}
+	explicit cost_flow_net(int n) : cost_flow_net(n, n - 1, n - 2) {}
+
+	void push_edge(int u, int v, T cap, T cost) {
+		g.push_edge(u, v, edges.size());
+		edges.push_back({cap, 0, cost});
+		g.push_edge(v, u, edges.size());
+		edges.push_back({0, 0, -cost});
+	}
+
+	std::pair<T, T> dinic_mcmf() {
+		T max_flow = 0, total_cost = 0;
+		std::vector<T> distance;
+		std::vector<int> visited(n), current_index(n);
+		auto spfa = [&]() {
+			distance.assign(n, cp_limits<T>::infinity());
+			std::vector<int> in_queue(n);
+			std::queue<int> q;
+			q.push(source);
+			distance[source] = 0;
+			in_queue[source] = 1;
+			while (!q.empty()) {
+				int u = q.front();
+				q.pop();
+				in_queue[u] = 0;
+				for (int i = 0; i < std::ssize(g[u]); i++) {
+					int ei = g.dat[u][i];
+					edge &e = edges[ei];
+					int v = g[u][i];
+					if (e.flow >= e.cap)
+						continue;
+					if (T new_dist = distance[u] + e.cost;
+					    new_dist < distance[v]) {
+						distance[v] = new_dist;
+						if (!in_queue[v])
+							q.push(v), in_queue[v] = 1;
+					}
+				}
+			}
+			return distance[sink] != cp_limits<T>::infinity();
+		};
+		auto dfs = [&](auto &self, int u, T flow) -> T {
+			if (u == sink || flow == 0)
+				return flow;
+			T current_flow = 0;
+			visited[u] = true;
+			for (int &i = current_index[u]; i < std::ssize(g[u]); i++) {
+				int ei = g.dat[u][i];
+				edge &e = edges[ei];
+				int v = g[u][i];
+				if (distance[v] != distance[u] + e.cost)
+					continue;
+				if (visited[v])
+					continue;
+				T can_push = self(
+				    self, v, std::min(flow - current_flow, e.cap - e.flow));
+				if (can_push > 0) {
+					e.flow += can_push;
+					edges[ei ^ 1].flow -= can_push;
+					current_flow += can_push;
+					total_cost += can_push * e.cost;
+					if (current_flow == flow)
+						break;
+				}
+			}
+			visited[u] = false;
+			return current_flow;
+		};
+		while (spfa()) {
+			current_index.assign(n, 0);
+			T current_flow;
+			while ((current_flow = dfs(dfs, source, cp_limits<T>::infinity())) >
+			       0)
+				max_flow += current_flow;
+		}
+		return {total_cost, max_flow};
+	}
+};
+
 #endif
--- flow_test.cpp
+++ flow_test.cpp
@@ -11,3 +11,15 @@ TEST_CASE("dinic max flow", "[flow]") {
 	mxf.push_edge(0, 2, 30);
 	REQUIRE(mxf.dinic_max_flow() == 50);
 }
+
+TEST_CASE("dinic ssp min cost max flow", "[flow]") {
+	cost_flow_net<int> mcmf(4, 3, 2);
+	mcmf.push_edge(3, 1, 30, 2);
+	mcmf.push_edge(3, 2, 20, 3);
+	mcmf.push_edge(1, 2, 20, 1);
+	mcmf.push_edge(1, 0, 30, 9);
+	mcmf.push_edge(0, 2, 30, 5);
+	auto [cost, flow] = mcmf.dinic_mcmf();
+	REQUIRE(flow == 50);
+	REQUIRE(cost == 280);
+}
