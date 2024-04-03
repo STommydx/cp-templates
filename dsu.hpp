6569e36aead27c29cdb0f8a06ce601a9e9111158
STommydx <dev@stdx.space>
2024-04-03 14:21:26 -0400

    feat(templates): dsu with data

--- dsu.hpp
+++ dsu.hpp
@@ -8,11 +8,13 @@
 
 #include <vector>
 
-class dsu : std::vector<int> {
+template <class T = void, class Op = std::plus<>> class dsu;
+
+template <class Op> class dsu<void, Op> : std::vector<int> {
   public:
 	static const int same_set = -1;
 
-	dsu(size_t n) : std::vector<int>(n, -1) {}
+	explicit dsu(size_t n) : std::vector<int>(n, -1) {}
 	int find_set(int x) {
 		return (*this)[x] >= 0 ? (*this)[x] = find_set((*this)[x]) : x;
 	}
@@ -26,6 +28,30 @@ class dsu : std::vector<int> {
 		(*this)[gv] += (*this)[gu];
 		(*this)[gu] = gv;
 		return gv;
+	}
+	size_t size() const { return std::vector<int>::size(); }
+};
+
+template <class T, class Op> class dsu : public dsu<void, Op> {
+	std::vector<T> dat;
+	Op op;
+
+  public:
+	using dsu<void, Op>::same_set;
+
+	explicit dsu(const std::vector<T> &v, const Op &op = {})
+	    : dsu<void, Op>(v.size()), dat(v), op(op) {}
+	explicit dsu(size_t n, const T &init = {}, const Op &op = {})
+	    : dsu(std::vector<T>(n, init), op) {}
+
+	using dsu<void, Op>::find_set;
+	T operator[](int x) { return dat[find_set(x)]; }
+	int union_sets(int u, int v) {
+		int gu = find_set(u), gv = find_set(v);
+		int result = dsu<void, Op>::union_sets(gu, gv);
+		if (result != same_set)
+			dat[result] = op(dat[result], dat[result ^ gu ^ gv]);
+		return result;
 	}
 };
 
--- dsu_test.cpp
+++ dsu_test.cpp
@@ -3,7 +3,7 @@
 #include <catch2/catch_test_macros.hpp>
 
 TEST_CASE("dsu behaves as expected", "[dsu]") {
-	dsu d(5);
+	dsu<> d(5);
 	SECTION("dsu constructed as expected") {
 		REQUIRE(d.find_set(0) == 0);
 		REQUIRE(d.find_set(1) == 1);
@@ -13,7 +13,7 @@ TEST_CASE("dsu behaves as expected", "[dsu]") {
 		REQUIRE(d.union_sets(0, 1) == 1); // same size, v is selected
 		REQUIRE(d.union_sets(1, 2) == 1); // larger component is selected
 		REQUIRE(d.union_sets(3, 4) == 4);
-		REQUIRE(d.union_sets(0, 2) == dsu::same_set);
+		REQUIRE(d.union_sets(0, 2) == dsu<>::same_set);
 	}
 	SECTION("dsu find_set behaves as expected") {
 		d.union_sets(0, 1);
@@ -32,4 +32,20 @@ TEST_CASE("dsu behaves as expected", "[dsu]") {
 		REQUIRE(d.size_of_set(3) == 2);
 		REQUIRE(d.size_of_set(4) == 2);
 	}
+}
+
+TEST_CASE("dsu with data", "[dsu]") {
+	std::vector<int> v{1, 2, 3, 4, 5};
+	dsu<int, std::multiplies<>> d(v);
+	d.union_sets(0, 1);
+	REQUIRE(d.size_of_set(1) == 2);
+	REQUIRE(d[1] == 2);
+	d.union_sets(1, 2);
+	d.union_sets(3, 4);
+	REQUIRE(d.size_of_set(1) == 3);
+	REQUIRE(d[1] == 6);
+	REQUIRE(d.size_of_set(3) == 2);
+	REQUIRE(d[3] == 20);
+	REQUIRE(d.size_of_set(4) == 2);
+	REQUIRE(d[4] == 20);
 }
